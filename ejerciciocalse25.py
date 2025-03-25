eac5d7a4115ac5fc8f4ee82beb05da12b6bf41b5
DiegoWhite99 <diegofer.cas.99@gmail.com>
2025-03-25 17:32:11 -0500

    ejercicio python

---
+++ ejerciciocalse25.py
@@ -0,0 +1,59 @@
+#Vamos a crear un programa que tenga el siguiente menú:
+#Añadir número a la lista: Me pide un número de la lista y lo añade al final de la lista.
+
+#Añadir número de la lista en una posición: Me pide un número y una posición, y si la posición existe en la lista lo añade a ella (la posición se pide a partir de 1).
+
+#Longitud de la lista: te muestra el número de elementos de la lista.12
+
+#Eliminar el último número: Muestra el último número de la lista y lo borra.
+
+#Eliminar un número: Pide una posición, y si la posición existe en la lista lo borra de ella (la posición se pide a partir de 1).
+
+#Contar números: Te pide un número y te dice cuantas apariciones hay en la lista.
+
+#Posiciones de un número: Te pide un número y te dice en que posiciones está (contando desde 1).
+
+#Mostrar números: Muestra los números de la lista
+
+
+listanumero = []
+
+while True:  # Pista: Necesitas repetir el menú
+    print("\nMenú:")
+    print("1. Añadir número a la lista")
+    print("2. Añadir número en una posición específica")
+    print("3. Mostrar la longitud de la lista")
+    print("4. Eliminar el último número")
+    print("5. Eliminar un número en una posición")
+    print("6. Contar ocurrencias de un número")
+    print("7. Mostrar posiciones de un número")
+    print("8. Mostrar la lista")
+    print("9. Salir")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

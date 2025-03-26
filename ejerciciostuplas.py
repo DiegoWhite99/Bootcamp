bf430ee48be3c27b8d662faa5a556347ed881d85
DiegoWhite99 <diegofer.cas.99@gmail.com>
2025-03-26 14:39:06 -0500

    funciones

---
+++ ejerciciostuplas.py
@@ -0,0 +1,11 @@
+datos = ()
+datos = (1, 2, 3, 4, 5, 6)
+datos2 = (100, "hola", "g", 10.23, ["a", "e", "o", "i", "u"], (10, 100))
+
+print(type(datos))  # Imprime el tipo de la variable 'datos' (tuple)
+print(datos)        # Imprime el contenido de la tupla 'datos'
+
+# Convertir la tupla 'datos' en una lista
+lista = list(datos)
+print(type(lista))  # Imprime el tipo de la variable 'lista' (list)
+print(lista)        # Imprime el contenido de la lista
